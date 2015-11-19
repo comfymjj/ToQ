37637afe3713bd66bc5cd9f9f191451620011a8b
katieyang <13hy22@queensu.ca>
2015-11-19 17:47:08 -0500

    First Commit

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseDrag()
+		
+	{
+		
+		Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		point.z = gameObject.transform.position.z;
+		
+		gameObject.transform.position = point;
+		
+		Cursor.visible = false;
+		
+		
+	}}
