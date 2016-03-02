005a16e70033e0d87dc07723f71e53bc0bf49322
Carter Cook <13cjcc@gmail.com>
2016-03-01 20:01:36 -0500

    reorganised visual novel, added "Create Visual Novel Setup" button

--- Assets/visualNovel/Editor/LinesEditor.cs
+++ Assets/visualNovel/Editor/LinesEditor.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
+using System.Text.RegularExpressions;
 
 [CustomEditor(typeof(Lines))]
 public class LinesEditor : Editor {
@@ -11,8 +12,146 @@ public class LinesEditor : Editor {
 	private SerializedProperty rightCharacter;
 	private float width1; //width of first item in reorderablelist
 
-	private void OnEnable() {
-		//script = (Lines)target; //the script this class is targeting
+	[MenuItem("GameObject/Create Visual Novel Setup")]
+	public static void CreateVisualNovel() {
+		PrefabUtility.InstantiatePrefab(Resources.Load("List of Lines"));
+		PrefabUtility.InstantiatePrefab(Resources.Load("background"));
+		PrefabUtility.InstantiatePrefab(Resources.Load("leftCharacter"));
+		PrefabUtility.InstantiatePrefab(Resources.Load("rightCharacter"));
+		((GameObject)PrefabUtility.InstantiatePrefab(Resources.Load("canvas"))).GetComponent<Canvas>().worldCamera = Camera.main;
+	}
+
+	void OnEnable() {
+		script = (Lines)target;
+
+		leftCharacter = serializedObject.FindProperty("leftCharacter");
+		rightCharacter = serializedObject.FindProperty("rightCharacter");
+	}
+
+	public override void OnInspectorGUI() {
+		serializedObject.Update();
+
+		//the .txt file we're regexing the lines from
+		SerializedProperty txt = serializedObject.FindProperty("txt");
+		EditorGUILayout.PropertyField(txt);
+
+		if (GUI.changed) {
+			string text = ((TextAsset)txt.objectReferenceValue).text;
+
+			string pattern =
+				@"\n(?<name>[^\n:]*): *" + //match beginning, e.g. "Prince:"
+				@"(?<dialogue>[^:]*)" + //match middle, i.e. dialogue
+				@"\n[^\n:]*:"; //match end, i.e. beginning of next line
+
+			MatchCollection matches = Regex.Matches(text, pattern); //get all regex matches
+
+			Debug.Log("GUI changed");
+
+			if (script.lines.Length != matches.Count) {
+				script.lines = new Line[matches.Count];
+			}
+
+			for (int i = 0; i < matches.Count; i++) {
+				script.lines[i].name = matches[i].Groups["name"].ToString();
+				script.lines[i].dialogue = matches[i].Groups["dialogue"].ToString();
+			}
+
+			serializedObject.Update();
+		}
+
+		EditorGUILayout.BeginHorizontal();
+		{
+			EditorGUILayout.PropertyField(leftCharacter, GUIContent.none);
+			EditorGUILayout.PropertyField(rightCharacter, GUIContent.none);
+		}
+		EditorGUILayout.EndHorizontal();
+
+		SerializedProperty fadeIn = serializedObject.FindProperty("fadeIn");
+		EditorGUILayout.PropertyField(fadeIn);
+
+		SerializedProperty lines = serializedObject.FindProperty("lines");
+
+		Debug.Log(lines.arraySize);
+
+		ArrayGUI(lines);
+
+		SerializedProperty fadeOut = serializedObject.FindProperty("fadeOut");
+		EditorGUILayout.PropertyField(fadeOut);
+
+		//wrap text in reorderablelist 
+		//EditorStyles.textField.wordWrap = true;
+		//run drawElementCallback
+		//reorderableList.DoLayoutList();
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	void ArrayGUI(SerializedProperty _property) {
+		int size = _property.arraySize;
+
+		for (int i = 0; i<size; i++) {
+			var element = _property.GetArrayElementAtIndex(i);
+
+			EditorGUILayout.BeginHorizontal();
+			{
+				//sprite
+				SerializedProperty spriteProp = element.FindPropertyRelative("sprite");
+				Sprite sprite = (Sprite)spriteProp.objectReferenceValue;
+
+				float height = 80; //chosen arbitrarily
+				float width; //width of the sprite thumbnail
+				if (sprite == null) {
+					width = height;
+				} else {
+					width = sprite.rect.width * (height / sprite.rect.height);
+					if (width > height) {
+						width = height;
+					}
+				}
+
+				Rect rect = EditorGUILayout.GetControlRect(GUILayout.Width(width), GUILayout.Height(height));
+				spriteProp.objectReferenceValue = (Sprite)EditorGUI.ObjectField(rect, sprite, typeof(Sprite), false);
+
+				EditorGUILayout.BeginVertical();
+				{
+					//speaker
+					EditorGUILayout.PropertyField(element.FindPropertyRelative("speaker"), GUIContent.none);
+
+					//text
+					string name = element.FindPropertyRelative("name").stringValue;
+					string dialogue = element.FindPropertyRelative("dialogue").stringValue;
+					EditorStyles.label.wordWrap = true;
+					EditorGUILayout.LabelField(name+": "+dialogue);
+				}
+				EditorGUILayout.EndVertical();
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private void stuff() {
+		script = (Lines)target; //the script this class is targeting
+		
 		//reorderableList = new ReorderableList(script.lines, typeof(string), true, true, true, true);
 		reorderableList = new ReorderableList(serializedObject, serializedObject.FindProperty("lines"), true, true, true, true); //a list of all lines
 		width1 = 90; //width of item 1, for easy reference
@@ -53,7 +192,7 @@ public class LinesEditor : Editor {
 			}
 			float xCenter = (rect.height - width2)/2; //used to center the sprite
 			spriteProp.objectReferenceValue = (Sprite)EditorGUI.ObjectField(new Rect(rect.x + xCenter, rect.y, width2, rect.height),
-				 sprite, typeof(Sprite));
+				 sprite, typeof(Sprite), false);
 
 			//draw next in line
 			rect.x += rect.height + 2;
@@ -67,8 +206,10 @@ public class LinesEditor : Editor {
 			if (EditorGUI.EndChangeCheck()) {
 				EditorUtility.SetDirty(target);
 			}
+			
 		};
-
+		
+		
 		//Now add listeners for the ReorderableList (it's kind of outdated so it doesn't update through onInpectorGUI)
 
 		// Draws the header of the list
@@ -114,25 +255,6 @@ public class LinesEditor : Editor {
 		// reorderableList = new ReorderableList(serializedObject, serializedObject.FindProperty("list"), true, true, true, true);
 	}
 
-	public override void OnInspectorGUI() {
-		//begin OnInspectorGUI
-		serializedObject.Update();
-		EditorGUILayout.BeginHorizontal();
-			EditorGUILayout.BeginVertical();
-				EditorGUILayout.LabelField("Left Character:");
-				EditorGUILayout.PropertyField(leftCharacter, GUIContent.none);
-			EditorGUILayout.EndVertical();
-			EditorGUILayout.BeginVertical();
-				EditorGUILayout.LabelField("Right Character:");
-				EditorGUILayout.PropertyField(rightCharacter, GUIContent.none);
-			EditorGUILayout.EndVertical();
-		EditorGUILayout.EndHorizontal();
+	
 
-		//wrap text in reorderablelist 
-		EditorStyles.textField.wordWrap = true;
-		//run drawElementCallback
-		reorderableList.DoLayoutList();
-		//end OnInpectorGUI
-		serializedObject.ApplyModifiedProperties();
-	}
 }
---
+++ Assets/visualNovel/scripts/Lines.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+//helper classes
+public enum Speaker { leftCharacter, rightCharacter };
+[System.Serializable]
+public struct Line {
+	public Speaker speaker;
+	public Sprite sprite;
+	public string name;
+	public string dialogue;
+}
+
+//main class
+public class Lines : MonoBehaviour {
+	public TextAsset txt; //a file, e.g. dialogue.txt
+	public Line[] lines; //array of all lines shown one-by-one in textbox
+	public SpriteRenderer leftCharacter;
+	public SpriteRenderer rightCharacter;
+	public Color fadeIn = Color.black;
+	public Color fadeOut = Color.black;
+
+	private string[] words; //array of lines broken up by word
+	private Text text; //actual text object in box
+	private int lineIndex; //the current line we're on
+	private int wordIndex; //the current word we're on
+
+	private enum Fade { In, Out, None }
+	private Fade fading;
+	private RectTransform textBox; //position of our textBox
+	private RectTransform nameBox; //position of the box in the textbox that displays names
+	private Text speakerName; //text showing name of current speaker
+	private RawImage fader; //used for fading in/out of black
+
+	void Start() {
+		//get game objects from the hierarchy
+		textBox = GameObject.Find("canvas/textBox").GetComponent<RectTransform>();
+		text = GameObject.Find("canvas/textBox/textHolder/text").GetComponent<Text>();
+		nameBox = GameObject.Find("canvas/textBox/nameBox").GetComponent<RectTransform>();
+		speakerName = GameObject.Find("canvas/textBox/nameBox/name").GetComponent<Text>();
+		fader = GameObject.Find("canvas/black").GetComponent<RawImage>();
+		speakerName.text = "";
+		text.text = "";
+
+		lineIndex = -1;
+
+		//start with fadeIn colour
+		fading = Fade.In;
+		fader.color = fadeIn;
+	}
+
+	//runs 60 times a second
+	void Update() {
+		//fades take away control until done
+		if (fading != Fade.None) {
+			fade();
+			return;
+		}
+
+		//go to next line on mouse click
+		if (Input.GetMouseButtonDown(0) || Input.anyKeyDown) {
+			nextLine();
+		}
+		Debug.Log(fader.color.a);
+		if (wordIndex < words.Length) { //print more words, unless all words are onscreen
+			text.text += words[wordIndex]+" ";
+			wordIndex++;
+		}
+	}
+
+	private void fade() {
+		switch (fading) {
+			case Fade.In:
+				fadeIn.a -= Time.deltaTime;
+				fader.color = fadeIn;
+				if (fadeIn.a <= 0) {
+					nextLine();
+					fading = Fade.None;
+				}
+				break;
+			case Fade.Out:
+				fadeOut.a += Time.deltaTime;
+				fader.color = fadeIn;
+				if (fadeOut.a >= 1) {
+					fading = Fade.None;
+					//TODO: go to next scene!
+				}
+				break;
+		}
+	}
+
+	private void nextLine() {
+		//go to the next line
+		lineIndex++;
+		//prep variables for new line
+		wordIndex = 0;
+		text.text = "";
+		if (lineIndex < lines.Length) {
+			words = lines[lineIndex].dialogue.Split(' '); //break up this line by word and store it in words
+
+			float offset = 0.3f; // a 0 to 1 value for how far nameBox.x is from the textbox's center 
+			speakerName.text = lines[lineIndex].name; //set name displayed onscreen
+
+			//set current speaker
+			if (lines[lineIndex].speaker == Speaker.leftCharacter) {
+				nameBox.anchoredPosition = new Vector2(-textBox.rect.width*offset, nameBox.anchoredPosition.y);
+				leftCharacter.color = Color.white;
+				leftCharacter.sprite = lines[lineIndex].sprite; //update character sprites
+				rightCharacter.color = new Color(.75f, .75f, .75f);
+			} else if (lines[lineIndex].speaker == Speaker.rightCharacter) {
+				nameBox.anchoredPosition = new Vector2(textBox.rect.width*offset, nameBox.anchoredPosition.y);
+				rightCharacter.color = Color.white;
+				rightCharacter.sprite = lines[lineIndex].sprite; //update character sprites
+				leftCharacter.color = new Color(.75f, .75f, .75f);
+			}
+		} else { //we have finished all lines
+			words = new string[]{""};
+			fading = Fade.Out;
+		}
+	}
+
+}
